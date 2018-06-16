7728d0dc742e41fc3ab5404c0f9754e3ac480117
milos2806 <milosvasiljevic28@gmail.com>
2018-06-16 16:12:26 +0200

    adds class Helper

--- src/framework/Helper.java
+++ src/framework/Helper.java
@@ -5,9 +5,6 @@ public class Helper {
 
     public static String getRandomText() {
 
-//        int random = (int) (Math.random() * 150 + 1);
-//        String randomText = "mv" + random;
-//        return randomText;
         return "mv" + (int) (Math.random() * 50 + 1);
     }
 

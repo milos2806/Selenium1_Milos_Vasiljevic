7ec29c829c1f2613d4aaa4fef7b539c31c036457
milos2806 <milosvasiljevic28@gmail.com>
2018-06-16 19:39:53 +0200

    main class has been completed

--- src/main.java
+++ src/main.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class main {
 
     public static void main(String[] args) throws InterruptedException {
-        
+
         //open chrome in maximize mode
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -25,75 +27,70 @@ public class main {
 
         WebElement loginButton = driver.findElement(By.className("btn-primary"));
         loginButton.click();
+        
+        
 
         //CATEGORIES
-        WebElement categoriesLink = driver.findElement(By.linkText("Categories"));
-        categoriesLink.click();
-        
-        System.out.println(driver.getTitle());
-        
-        // clickk on  add category button
-        WebElement addCategoryButton = driver.findElement(By.className("pull-right"));
+        WebElement navBarCat = driver.findElement(By.className("navbar-nav"));
+        List<WebElement> liTagsCat = navBarCat.findElements(By.tagName("li"));
+        liTagsCat.get(2).click();
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        WebDriverWait waitCat = new WebDriverWait(driver, 7);
+        WebElement addCategoryButton = waitCat.until(ExpectedConditions.elementToBeClickable(By.className("pull-right")));
         addCategoryButton.click();
-        
-        //fill title
+
         WebElement titleField = driver.findElement(By.id("title"));
-        titleField.sendKeys(Helper.getRandomText());
-        
-        // save new category
+        titleField.sendKeys(Helper.getRandomTextCat());
+
         WebElement saveButton = driver.findElement(By.id("save-category-button"));
         saveButton.click();
-
-        // go to REGIONS page
-        WebElement navBar = driver.findElement(By.className("navbar-nav"));
-        List<WebElement> liTags = navBar.findElements(By.tagName("li"));
-        liTags.get(3).click();
-
-        System.out.println(driver.getTitle());
-
-        // clickk on  add region button
-        WebElement addRegionButton = driver.findElement(By.className("pull-right"));
-        addRegionButton.click();
         
-        //fill title
+        
+
+        // REGIONS 
+        WebElement navBarReg = driver.findElement(By.className("navbar-nav"));
+        List<WebElement> liTagsReg = navBarReg.findElements(By.tagName("li"));
+        liTagsReg.get(3).click();
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        WebDriverWait waitReg = new WebDriverWait(driver, 6);
+        WebElement addRegionsButton = waitReg.until(ExpectedConditions.elementToBeClickable(By.className("pull-right")));
+        addRegionsButton.click();
+
         WebElement titleField1 = driver.findElement(By.id("title"));
-        titleField1.sendKeys(Helper.getRandomText());
-        
-        // save new region
+        titleField1.sendKeys(Helper.getRandomTextReg());
+
         WebElement saveRegionButton = driver.findElement(By.id("save-region-button"));
         saveRegionButton.click();
         
-        // go to PORTALS page
-        WebElement portalsLink = driver.findElement(By.linkText("Portals"));
-        portalsLink.click();
+        
+
+        //PORTALS 
+        WebElement navBarPor = driver.findElement(By.className("navbar-nav"));
+        List<WebElement> liTagsPor = navBarPor.findElements(By.tagName("li"));
+        liTagsPor.get(4).click();
 
         // print title of web page in console
-        System.out.println(driver.getTitle());
+        System.out.println("Page title is: " + driver.getTitle());
 
-        // clickk on  add PORTAL button
-        WebElement addPortalButton = driver.findElement(By.className("pull-right"));
+        WebDriverWait waitPor = new WebDriverWait(driver, 5);
+        WebElement addPortalButton = waitPor.until(ExpectedConditions.elementToBeClickable(By.className("pull-right")));
         addPortalButton.click();
-        
-        //fill title
+
         WebElement titlePortalField = driver.findElement(By.id("title"));
-        titlePortalField.sendKeys(Helper.getRandomText());
-        
-        //fill URL
+        titlePortalField.sendKeys(Helper.getRandomTextPor());
+
         WebElement urlField = driver.findElement(By.id("url"));
         urlField.sendKeys("http://test.rs");
-        
-        // save new region and url
+
         WebElement savePortalButton = driver.findElement(By.id("save-portal-button"));
         savePortalButton.click();
-        
-        
-        Thread.sleep(6000);
-        driver.quit();
-        
-        
-        
-        
 
+        Thread.sleep(5000);
+        driver.quit();
     }
 
 }

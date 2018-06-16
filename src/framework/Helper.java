a7dd7e4e646c568f621d5b1e984622c645531438
milos2806 <milosvasiljevic28@gmail.com>
2018-06-16 19:40:36 +0200

    added Helper class

--- src/framework/Helper.java
+++ src/framework/Helper.java
@@ -1,13 +1,18 @@
-
 package framework;
 
 public class Helper {
 
-    public static String getRandomText() {
+    public static String getRandomTextCat() {
 
-        return "mv" + (int) (Math.random() * 50 + 1);
+        return "mvCat" + (int) (Math.random() * 150 + 1);
     }
 
-    
-}
+    public static String getRandomTextReg() {
+        return "mvReg" + (int) (Math.random() * 200 + 1);
+    }
 
+    public static String getRandomTextPor() {
+        return "mvPor" + (int) (Math.random() * 250 + 1);
+    }
+
+}
